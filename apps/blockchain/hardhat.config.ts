3f46da81ee8202d1211f60e78e995d13ff59c279
Michael Stipsits <michael.stipists@gmx.at>
2023-01-06 19:49:53 +0100

    added deployment to goerli network

--- apps/blockchain/hardhat.config.ts
+++ apps/blockchain/hardhat.config.ts
@@ -6,3 +6,16 @@ const config: HardhatUserConfig = {
 };
 
 export default config;
+const ALCHEMY_API_KEY = "bECA8NJh3swcK6Cfi2QwWqqAkXVB7qSm";
+
+const GOERLI_PRIVATE_KEY = "b1eb87fb742fcd1310fb8ee1f54203c7e88c9216775fe5e99dc3d382854a67a9";
+
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
+    }
+  }
+};
